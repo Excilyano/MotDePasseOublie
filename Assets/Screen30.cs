7e767418d949b01bf1a6338d941b86b08c32f561
Argann Bonneau <argann.b@hotmail.fr>
2021-01-30 20:37:21 +0100

    Screen 30 + 31 + DOTween

---
+++ Assets/Screen30.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class Screen30 : MonoBehaviour
+{
+    public RectTransform aceCursor;
+
+    public Toggle toggle;
+
+    Vector2 startCursorPos;
+
+    void Start()
+    {
+        startCursorPos = aceCursor.position;
+
+        Sequence sequence = DOTween.Sequence()
+            .AppendInterval(3)
+            .Append(
+                aceCursor.DOMove(toggle.transform.Find("Background").position, 1f)
+            )
+            .AppendInterval(0.5f)
+            .Append(
+                aceCursor.DOPunchAnchorPos(Vector2.down, 0.5f)
+            )
+            .AppendCallback(() => {
+                toggle.isOn = true;
+            })
+            .AppendInterval(0.5f)
+            .Append(
+                aceCursor.DOMove(startCursorPos, 1f)
+            )
+            .Play();
+    }
+}
