ce656cef702fbf0948b47192075be46fc4734209
Argann Bonneau <argann.b@hotmail.fr>
2021-01-30 20:19:08 +0100

    Screen 32

---
+++ Assets/EnvoiAuServeur32.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnvoiAuServeur32 : MonoBehaviour
+{
+    public TextMeshProUGUI textSending;
+
+    public TextMeshProUGUI reussite;
+
+    public Button continueButton;
+
+
+    string startText;
+
+    void Start()
+    {
+        startText = textSending.text;
+        StartCoroutine(Sequence());
+        reussite.gameObject.SetActive(false);
+        continueButton.interactable = false;
+
+    }
+
+    IEnumerator Sequence()
+    {
+        for (int i = 0; i < 24; i++)
+        {
+            textSending.text = startText + new string('.', (i % 3)+1);
+            yield return new WaitForSeconds(0.25f);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        reussite.gameObject.SetActive(true);
+        continueButton.interactable = true;
+    }
+}
