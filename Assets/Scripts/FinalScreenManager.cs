22a56b531dc208416d039c28a7e413cc6adf88b5
Excilyano <agahnim13@gmail.com>
2021-01-31 15:27:57 +0100

    ajout de l'ecran de redemarrage

---
+++ Assets/Scripts/FinalScreenManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalScreenManager : MonoBehaviour
+{
+    public GameObject reboot;
+    public GameObject bluescreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reboot.SetActive(true);
+        bluescreen.SetActive(false);
+        GetComponent<PressAnyKeyToQuit>().enabled = false;
+
+        StartCoroutine(BlueScreenAppear());
+    }
+
+    IEnumerator BlueScreenAppear() {
+        yield return new WaitForSeconds(8f);
+        reboot.SetActive(false);
+        bluescreen.SetActive(true);
+
+        GetComponent<PressAnyKeyToQuit>().enabled = true;
+    }
+
+
+}
